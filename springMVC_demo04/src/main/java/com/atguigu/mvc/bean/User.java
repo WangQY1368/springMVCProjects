fe6a6424665b624bdf6d8d55d01974a4b9b8e6e7
WangQY1368 <289924569@qq.com>
2022-08-26 18:27:10 +0800

    完成了demo04

---
+++ springMVC_demo04/src/main/java/com/atguigu/mvc/bean/User.java
@@ -0,0 +1,53 @@
+package com.atguigu.mvc.bean;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    public String password;
+
+    public Integer age;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password, Integer age) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
