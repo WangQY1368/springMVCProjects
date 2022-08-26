0636b3a075b015d3910e1b03bb91d3b79a8de11d
WangQY1368 <289924569@qq.com>
2022-08-26 18:28:50 +0800

    完成了rest

---
+++ springMVC_rest/src/main/java/com/atguigu/mvc/controller/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.atguigu.mvc.controller;
+
+import com.atguigu.mvc.bean.Employee;
+import com.atguigu.mvc.dao.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    //查询所有员工信息
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getEmployeeList(Model model){
+        Collection<Employee> employeeList = employeeDao.getAll();
+        model.addAttribute("employeeList", employeeList);
+        return "employee_list"; }
+
+    //删除员工信息
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
+
+    //添加员工信息
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee", employee);
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+}
